904edbe32e2d1abbef4fa795512c50f06ee34bb2
sathishrcb08-art <sathish.rcb08@gmail.com>
2026-08-08 21:16:16 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+  public static void main(string[] args) {
+    int num=15;
+    if(num %2==0) {
+      System.out.println(num+"is Even");
+    } else {
+      System.out.println(num+"is Odd");
+    }
+  }
+}
